4030b3ba926349d6cd6af17320c9034617faef40
Feecon <mail2feeconbehera@gmail.com>
2024-08-23 04:19:49 +0530

    sasa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
 
         <Routes>
           <Route path='/home' element={<Home />}></Route>
+          <Route path='/' element={<Home />}></Route>
           <Route path='/about' element={<About />}></Route>
           <Route path='/contact' element={<Contact />}></Route>
           <Route path='/project' element={<Project />}></Route>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,37 +3,40 @@ import {Linkedin, Github, PinInterest, DribbleIcon, Twitter} from './icon'
 
 const Header = () => {
     return (
-        <header className='w-full px-32 py-8 font-medium text-dark flex items-center justify-between shadow-sm'>
+        <header className='w-full px-32 py-8 font-medium text-dark flex items-center justify-between'>
             {/* Navigation links starts */}
             <nav>
                 <ul>
-                    <NavLink to={'/home'} className='mr-4'>Home</NavLink>
+                    <NavLink to={'/'} className='mr-4'>Home</NavLink>
                     <NavLink to={'/about'} className='mx-4'>About</NavLink>
                     <NavLink to={'/contact'} className='mx-4'>Contact</NavLink>
                     <NavLink to={'/project'} className='ml-4'>Project</NavLink>
                 </ul>
             </nav>
-            <nav className='flex'>
-                <a href="https://github.com/feecon12" className='mr-3 w-6'>
-                    <Github/>
-                </a>
-                <a href="https://www.linkedin.com/in/feecon-behera" className='mx-3 w-6'>
-                    <Linkedin/>
-                </a>
-                <a href="https://www.linkedin.com/in/feecon-behera" className='mx-3 w-6'>
-                    <PinInterest/>
-                </a>
-                <a href="https://www.linkedin.com/in/feecon-behera" className='mx-3 w-6'>
-                    <DribbleIcon/>
-                </a>
-                <a href="https://www.linkedin.com/in/feecon-behera" className='mx-3 w-6'>
-                    <Twitter/>
-                </a>
+            <div className='flex px-8 py-4'>
+                <nav className='flex items-center justify-between'>
+                    <a href="https://github.com/feecon12" className='mr-3 w-6'>
+                        <Github />
+                    </a>
+                    <a href="https://www.linkedin.com/in/feecon-behera" className='mx-3 w-6'>
+                        <Linkedin />
+                    </a>
+                    <a href="https://www.linkedin.com/in/feecon-behera" className='mx-3 w-6'>
+                        <PinInterest />
+                    </a>
+                    <a href="https://www.linkedin.com/in/feecon-behera" className='mx-3 w-6'>
+                        <DribbleIcon />
+                    </a>
+                    <a href="https://www.linkedin.com/in/feecon-behera" className='ml-3 w-6'>
+                        <Twitter />
+                    </a>
 
-                {/* <button type='submit' className='ml-3'>
+                    {/* <button type='submit' className='ml-3'>
                     Dark/Light
                 </button> */}
-            </nav>
+                </nav>
+            </div>
+            
 
         </header>
     )
